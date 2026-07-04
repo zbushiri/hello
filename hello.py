2f0aadea8c19db42822d3e5eba0a21b60c6d73dc
zbushiri <bushirizaki@gmail.com>
2026-07-04 12:13:40 -0400

    split input into first, last

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 # Ask user for their name whilst removing whitespace and capitalizes user's name
 name = input("What is your name? ").strip().title()
 
+# Split user's name into first and last name
+first, last = name.split(" ")
+
 # Say hello to the user
-print(f"hello, {name}")
+print(f"hello, {first}")
