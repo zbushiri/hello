cb7f61ed8a4f5d4a1b0586d426291e0d422381fc
Zaki <bushirizaki@gmail.com>
2026-07-05 14:56:39 -0400

    using def

--- Week 0/hello.py
+++ Week 0/hello.py
@@ -1,8 +1,10 @@
-# Ask user for their name whilst removing whitespace and capitalizes user's name
-name = input("What is your name? ").strip().title()
+def main():
+    name = input("What's your name? ")
+    hello(name)
 
-# Split user's name into first and last name
-first, last = name.split(" ")
 
-# Say hello to the user
-print(f"hello, {first}")
+def hello(to="world"):
+    print("hello,", to)
+
+
+main()
